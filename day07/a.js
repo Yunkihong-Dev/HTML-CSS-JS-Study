9423cfcfdf4411ab531cd3aed5bb7144a89d64ee
Yunkihong-Dev <81303136+Yunkihong-Dev@users.noreply.github.com>
2023-04-29 01:34:53 +0900

    Add files via upload

---
+++ day07/a.js
@@ -0,0 +1,11 @@
+      var person = {
+        name:'hong',
+        password : 'reeee',
+        AAAAAA: function(){
+          console.log(`${this.name} \'s password is ${this.password}.`);
+        }
+      };
+      var a=  true;
+     var msg="";
+     if(a) msg= "fuck";
+     person.AAAAAA;
